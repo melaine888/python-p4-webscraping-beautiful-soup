b7ce58d24195cde36b171a4b2f2ee4f1c9328583
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-06-25 11:08:51 +0300

    completed assignment

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,16 @@
-from turtle import ht
+
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+html = requests.get("https://moringaschool.com/", headers=headers)
+
+
+doc = BeautifulSoup(html.text, 'html.parser')
+print(doc.select('.heading financier'))
+print(doc.select('.heading financier')[0].contents)
+
+courses = doc.select('.heading-60-black.color-black.mb-20')
+
+for course in courses:
+    print(course.contents[0].strip())
